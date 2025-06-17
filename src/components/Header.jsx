da0966bd2bdb9287d11f5be880cd426884a872c6
KREZOO <krezoo.yt@gmail.com>
2025-06-17 12:25:41 +0300

    test-01

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Styles from './Styles.module.scss';
+
+const Header = () => {
+  return (
+    <header className={Styles.header}>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid velit
+        non pariatur numquam excepturi esse nihil molestiae! Accusantium
+        architecto dolor hic corrupti, facilis similique provident amet tempora
+        vero. Sit, minima!
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
